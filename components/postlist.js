52c437fc67e451b7f97b2a2c113eb8ed6deb26bb
Abdulai Musah <abdulaimusah31@gmail.com>
2022-08-02 06:39:06 +0000

    udated about page

--- components/postlist.js
+++ components/postlist.js
@@ -24,7 +24,7 @@ const PostList = ({posts}) => {
                     to-violet-500">
                        Popular Posts
                     </span>
-                </div>
+                </div>tt
                 <div className="flex flex-col">
                 {posts.length && posts.map(post => (
                     
